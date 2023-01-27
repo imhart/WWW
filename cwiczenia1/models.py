0f79a8948c60928d8c1de4132abb013f3e655320
imhart <danielnieznanski@op.pl>
2023-01-27 10:40:57 +0100

    lab2

--- cwiczenia1/models.py
+++ cwiczenia1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class book(models.Model):
+    def __str__(self):
+        return self.nazwa
 
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+    cena = models.DecimalField(max_digits=12, decimal_places=2)
 # Create your models here.
