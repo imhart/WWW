f9a27c5c5f334d69b46e4391ce1f1b1b5873572d
imhart <danielnieznanski@op.pl>
2023-01-27 10:52:10 +0100

    zadania 1-5

--- cwiczenia1/models.py
+++ cwiczenia1/models.py
@@ -1,9 +1,28 @@
 from django.db import models
-class book(models.Model):
-    def __str__(self):
-        return self.nazwa
+from django.utils.translation import gettext_lazy as _
+# kod z oficjalnej dokumentacji Django
+class Osoba(models.Model):
+    imie = models.CharField(max_length=60 ,blank=False)
+    nazwisko = models.CharField(max_length=60, blank=False)
+    class Months(models.TextChoices):
+        STYCZEN = 'I', _('Styczeń')
+        LUTY = 'II', _('Luty')
+        MARZEC = 'III', _('Marzec')
+        KWIECIEN = 'IV', _('Kwiecień')
+        MAJ = 'V', _('Maj')
+        CZERWIEC = 'VI', _('Czerwiec')
+        LIPIEC = 'VII', _('Lipiec')
+        SIERPIEN = 'VIII', _('Sierpień')
+        WRZESIEN = 'IX', _('Wrzesień')
+        PAZDZIERNIK = 'X', _('Październik')
+        LISTOPAD = 'XI', _('Listopad')
+        GRUDZIEN = 'XII', _('Grudzień')
 
-    nazwa = models.CharField(max_length=100)
-    opis = models.TextField(blank=True)
-    cena = models.DecimalField(max_digits=12, decimal_places=2)
+    miesiac_urodzenia = models.CharField(max_length=4,choices=Months.choices, default=Months.LUTY)
+    data_dodania = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.imie
+
+
 # Create your models here.
